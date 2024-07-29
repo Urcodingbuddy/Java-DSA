cb603c44664762887d16d9ad98ce594cd140f69a
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-30 00:05:23 +0530

    ansh package Created

---
+++ Day-1/com/ansh/Main.java
@@ -0,0 +1,10 @@
+package com.ansh;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+          System.out.println(args[0]);
+    }
+}
---
+++ Day-1/com/bro/Demo.java
@@ -0,0 +1,8 @@
+package com.bro;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hey World");
+    }
+}
+
