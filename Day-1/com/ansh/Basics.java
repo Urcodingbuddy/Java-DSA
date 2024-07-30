6252c87a035d75d1fdd7b821884b711693776f05
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-30 18:35:24 +0530

    while and for loop

---
+++ Day-1/com/ansh/Basics.java
@@ -0,0 +1,23 @@
+package com.ansh;
+
+public class Basics {
+    public static void main(String[] args) {
+        // int a = 10;
+        // if(a==10){
+        //     System.out.println("Hello World");
+        // }
+        // else{
+        //     System.out.println("Goodbye World");
+        // }
+
+        // int count = 1;
+        // while(count != 5){
+        //     System.out.println(count);
+        //     count++;
+        // }
+        // int i = 1;
+        for(int i= 1;i<=20;i++){
+            System.out.println(i);
+        }
+    }
+}
