369e587779efe91eeec44fcaef9aeee9e984edab
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-30 12:33:12 +0530

    data types done

---
+++ Day-1/com/ansh/premitive.java
@@ -0,0 +1,14 @@
+package com.ansh;
+public class premitive {
+    public static void main(String[] args) {
+        int rollno = 10;  // 4 byts
+        String name = "ANsh"; // 
+        char letter = 'r';
+        float marks = 95.5f; // 4 bytes
+        double largedecNum = 3564314.185441; // 8 bytes
+        long largeintValue = 6534635443654634L; // 8 bytes
+        boolean check = true; // True or false  
+
+
+    }
+}
