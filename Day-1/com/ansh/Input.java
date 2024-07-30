e6854c88d3a0dedeaade8ee006eccf8f8f0ad6eb
Urcodingbuddy <anshpethe110@gmail.com>
2024-07-30 13:04:38 +0530

    input and Scanner Class

---
+++ Day-1/com/ansh/Input.java
@@ -0,0 +1,22 @@
+package com.ansh;
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // System.out.print("Enter Your Roll num: ");
+        // int rollno = input.nextInt();
+        // System.out.println("Your Roll number is: "+ rollno);
+        // int a  = 234_000_000;
+        // System.out.println("Value of a: "+ a);
+        
+        // System.out.print("Enter Your Name: ");
+        // String name = input.nextLine();
+        // System.out.println("Your Name is: "+ name);
+        
+        System.out.print("Enter Your marks: ");
+        float marks = input.nextFloat();
+        System.out.println("Your Marks are: "+ marks);
+        
+    }
+}
