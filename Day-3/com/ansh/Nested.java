7926986eb2c096ded75e2f2a4793f27262b4f75d
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-02 22:02:14 +0530

    Nested condtion and Switch

---
+++ Day-3/com/ansh/Nested.java
@@ -0,0 +1,27 @@
+package ansh;
+
+import java.util.Scanner;
+
+public class Nested {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            int empID = in.nextInt();
+            String department = in.next();
+            
+            switch(empID){
+                case 1 -> System.out.print("Ansh");
+                case 2 -> System.out.print("Pethe");
+                case 3 -> {
+                    System.out.println("Emp Number 3");
+                    switch(department){
+                        case "IT" -> System.out.print("IT Department");
+                        case "Managment" -> System.out.print("Managment Department");
+                        default -> System.out.print("Invalid Department");
+                    }
+                   
+                }
+                default -> System.out.print("Invalid Emp ID :)");
+
+                }
+        }
+}
