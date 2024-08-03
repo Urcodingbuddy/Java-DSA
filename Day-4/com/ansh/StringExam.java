a94bbe40307a17edac9abcfb091d55a9ba8334b1
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-03 13:19:10 +0530

    String Done

---
+++ Day-4/com/ansh/StringExam.java
@@ -0,0 +1,12 @@
+package ansh;
+
+public class StringExam {
+        public static void main(String[] args) {
+            String msg = greet();
+            System.out.println(msg);
+        }
+        static String greet(){
+            String greeting  = "how are you"; 
+            return greeting;
+        }
+}
