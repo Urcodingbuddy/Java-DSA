c7188244d9247bac9ca81fd62308c8550e91f8a1
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-07 21:40:03 +0530

    Starting up

---
+++ Day-5/com/ansh/Main.java
@@ -0,0 +1,15 @@
+package com.ansh;
+
+public class Main {
+    public static void main(String[] args) {
+        // int []arr = new int[5];
+        // // or 
+        int[] arr2 = {1, 2, 3, 4, 5};
+        int[] ros;
+        ros = new int[45];
+          
+        System.out.println(arr2[4]); 
+        System.out.println(ros[4]); 
+
+    }
+}
