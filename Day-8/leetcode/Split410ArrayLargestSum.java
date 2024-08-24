cb5998225684fd96751134f04ed63e44b00ea7ea
Urcodingbuddy <anshpethe110@gmail.com>
2024-08-25 02:17:34 +0530

    Spliting Array with Largtest Sum

---
+++ Day-8/leetcode/Split410ArrayLargestSum.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class Split410ArrayLargestSum {
+    public static void main(String[] args) {
+        int[] nums = {7,2,5,10,8};
+        int k;
+    }
+    public int splitArray(int[] nums, int k) {
+        int start = 0;
+        int end  = 0;
+        for(int i = 0;i<nums.length;i++){
+            start = Math.max(start, nums[i]);
+            end += nums[i];
+        }
+        while(start<end){
+            int mid = start + (end - start)/2;
+            int sum = 0;
+            int pieces = 1;
+            for(int num: nums){
+                if((sum + num) > mid){
+                    sum = num;
+                    pieces++;
+                }else{
+                    start += num;
+                }
+            }
+            if(pieces > k){
+                start = mid + 1;
+            }else{
+                end += sum;
+            }
+        }
+        return start;
+    }
+}
+
